2ebf93ca60ddfebd3eea65fce4931d759aeb91a8
5usan <susanshrestha2056@gmail.com>
2022-07-24 21:54:01 +0545

    Implemented redux for creating contact

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { Provider } from "react-redux";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Login from "./components/Login/Login";
 import useLocalStorage from "./hooks/useLocalStorage";
+import { store } from "./store/store";
 
 function App() {
   const [id, setId] = useLocalStorage("id");
+  const dashboard = (
+    <Provider store={store}>
+      <Dashboard id={id} />
+    </Provider>
+  );
 
-  return id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />;
+  return id ? dashboard : <Login onIdSubmit={setId} />;
 }
 
 export default App;
