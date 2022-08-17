4eb0e629b3704b8361edbc025d35d5ff5d9d5f58
5usan <susanshrestha2056@gmail.com>
2022-08-17 22:05:52 +0545

    added contacts in contact section

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { ListGroup } from "react-bootstrap";
+import { useContacts } from "../../contexts/ContactsProvider";
 
 const Contacts = () => {
+  const { contacts } = useContacts();
   return (
-    <div>Contacts</div>
-  )
-}
+    <ListGroup variant="flush">
+      {contacts.map((contact) => {
+        return (
+          <ListGroup.Item key={contact.id}>{contact.name}</ListGroup.Item>
+        );
+      })}
+    </ListGroup>
+  );
+};
 
-export default Contacts
+export default Contacts;
