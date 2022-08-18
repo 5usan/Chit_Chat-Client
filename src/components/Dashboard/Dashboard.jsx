548dcc0f069bd3634fb9dd424145345bc284baf0
5usan <susanshrestha2056@gmail.com>
2022-08-18 10:28:40 +0545

    almost completed conversation section

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Sidebar from "../Sidebar/Sidebar";
+import OpenConversation from "../OpenConversation/OpenConversation";
+import { useConversations } from "../../contexts/ConversationsProvider";
 
 const Dashboard = ({ id }) => {
+  const { selectedConversation } = useConversations();
   return (
     <div style={{ height: "100vh" }} className="d-flex">
       <Sidebar id={id} />
+      {selectedConversation && <OpenConversation />}
     </div>
   );
 };
